8c77f01cd554790460e7dc6343216325ee90aefe
unknown <denizraj111@gmail.com>
2025-09-19 11:46:30 +0530

    Describe your changes

--- prog.php
+++ prog.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "hello";
+function greet($name) {
+    echo "Hello, $name! Welcome to PHP.<br>";
+}
 
-?>print"hello"
+
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+
+
+greet("Alice");
+
+$sum = addNumbers(5, 7);
+echo "The sum of 5 and 7 is: $sum";
+?>
